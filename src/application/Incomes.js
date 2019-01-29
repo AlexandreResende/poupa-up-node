603a602c58d8dfc1dc0573d57f3369467ec1c937
AlexandreResende <alexandre.amres@gmail.com>
2019-01-28 22:01:26 -0200

    chore: Create incomes endpoints and applications functions

--- src/application/Incomes.js
+++ src/application/Incomes.js
@@ -1,9 +1,25 @@
 
 const Incomes = () => ({
   getAllIncomes(req, res) {
-    console.log('Inside application layer');
     res.status(200).json({ message: 'Working controller '});
   },
+
+  getMonthlyIncomes(req, res) {
+    res.status(200).json({ message: 'Working controller2 '});
+  },
+
+  create(req, res) {
+    res.status(200).json({ message: 'Working controller3 '});
+  },
+
+  update(req, res) {
+    res.status(200).json({ message: 'Working controller4 '});
+  },
+
+  delete(req, res) {
+    res.status(200).json({ message: 'Working controller5 '});
+  }
+
 });
 
 module.exports = Incomes;
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,16 @@ const container = require('./infrastructure/container');
   res.status(200).json({ message: 'Working' });
 }); */
 
-router.get('/', container.resolve('Incomes').getAllIncomes);
+router.get('/incomes/get-all-incomes', container.resolve('Incomes').getAllIncomes);
+router.get('/incomes/get-monthly-incomes', container.resolve('Incomes').getMonthlyIncomes);
+router.post('/incomes/create', container.resolve('Incomes').create);
+router.put('/incomes/update', container.resolve('Incomes').update);
+router.delete('/incomes/delete', container.resolve('Incomes').delete);
+
+/* router.get('/expenses/get-all-expenses', container.resolve('Expenses').getAllExpenses);
+router.get('/expenses/get-monthly-expenses', container.resolve('Expenses').getMonthlyExpenses);
+router.post('expensess/create', container.resolve('Expenses').create);
+router.put('/expenses/update', container.resolve('Expenses').update);
+router.delete('/expenses/delete', container.resolve('Expenses').delete); */
 
 module.exports = router;
