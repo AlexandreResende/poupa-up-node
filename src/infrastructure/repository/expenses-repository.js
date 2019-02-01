7740250385c673117085ad73868cf4407b085cc4
AlexandreResende <alexandre.amres@gmail.com>
2019-02-01 20:30:39 -0200

    refactor: Implement expenses-repository methods

--- src/infrastructure/repository/expenses-repository.js
+++ src/infrastructure/repository/expenses-repository.js
@@ -1,15 +1,39 @@
+
+const ExpensesModel = require('../model/expenses');
+
 module.exports = class Expenses {
   constructor({ knexInstance }) {
     this.model = ExpensesModel.bindKnex(knexInstance);
   }
 
-  getAllIncomes() {}
+  async getAllExpenses() {
+    const result = await this.model.query().where({});
 
-  getMonthlyIncomes() {}
+    return result;
+  }
 
-  create() {}
+  async getMonthlyExpenses(month, year) {
+    const result = await this.model.query().where({
+      month,
+      year,
+    });
+  }
 
-  update() {}
+  async create(expensesData) {
+    const result = await this.model.query().insertAndFetch(expensesData);
 
-  delete() {}
+    return result;
+  }
+
+  async update({ id, ...rest}) {
+    const result = await this.model.query().updateAndFetchById(id, rest);
+
+    return result;
+  }
+
+  async delete(id) {
+    const result = await this.model.query().deleteById(id);
+
+    return result;
+  }
 }
