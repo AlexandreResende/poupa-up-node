c69f6a12251087e92a277df2ae689eb83a113604
AlexandreResende <alexandre.amres@gmail.com>
2019-02-01 22:53:14 -0200

    migrations: Implement incomes and expenses migrations

--- src/infrastructure/migrations/20190201222901_incomes.js
+++ src/infrastructure/migrations/20190201222901_incomes.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('incomes', (table) => {
+      table.increments('id').primary();
+      table.string('month', 2);
+      table.string('year', 4);
+      table.string('category', 100);
+      table.text('description');
+      table.decimal('value', 2)
+    }),
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('incomes'),
+  ]);
 };
