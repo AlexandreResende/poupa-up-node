057ca0e0ef34cd4e552159b3d03ab3bd74fe94d1
AlexandreResende <alexandre.amres@gmail.com>
2019-02-03 22:56:20 -0200

    chore: Create incomes-entity

---
+++ src/domain/entities/incomes-entity.js
@@ -0,0 +1,31 @@
+
+const Joi = require('joi');
+
+const { Model } = require('objection');
+
+const schema = Joi.object().keys({
+  value: Joi.number(),
+	description: Joi.string().max(255),
+	category: Joi.string().max(20),
+	month: Joi.string().max(2),
+  year: Joi.string().max(4),
+  createdAt: Joi.date(),
+  updatedAt: Joi.date().allow(null),
+});
+
+class IncomesEntity extends Model {
+  constructor(data) {
+    super();
+    const { value, error } = Joi.validate(data, schema, { abortEarly: true, stripUnknown: true });
+
+    if (error) throw TypeError(error);
+
+    Object.assign(this, data);
+  }
+
+  static get schema() {
+    return schema;
+  }
+}
+
+module.exports = IncomesEntity;
