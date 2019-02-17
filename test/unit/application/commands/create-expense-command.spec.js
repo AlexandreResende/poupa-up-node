fa104a02f04741877f65cb43f74f799d42587c38
AlexandreResende <alexandre.amres@gmail.com>
2019-02-16 23:32:23 -0200

    test: Create first test of the project

---
+++ test/unit/application/commands/create-expense-command.spec.js
@@ -0,0 +1,33 @@
+
+const CreateExpenseCommand = require('../../../../src/application/commands/create-expense-command');
+
+const sinon = require('sinon');
+
+const { expect } = require('chai');
+const { response } = require('stream');
+
+describe('CreateExpenseCommand', () => {
+  it('should return a new expense', async () => {
+    // given
+    const newExpense = {
+      value: 300,
+      description: 'A test expense',
+      category: 'FOOD',
+      month: '05',
+      year: '2019',
+    };
+    const stubs = {
+      expensesRepository: {
+        create: sinon.stub().resolves(newExpense),
+      }
+    };
+    const command = new CreateExpenseCommand(stubs);
+
+    // when
+    const result = await command.execute({ body: newExpense }, response);
+
+    console.log(result);
+
+    expect(result).to.be.like(newExpense);
+  });
+});
