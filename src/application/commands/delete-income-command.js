54dc45702a74b79d888e8395304f91a7514f27f1
AlexandreResende <alexandre.amres@gmail.com>
2019-02-20 23:10:41 -0300

    refactor: Adjust delete-income-command

--- src/application/commands/delete-income-command.js
+++ src/application/commands/delete-income-command.js
@@ -3,16 +3,10 @@ module.exports = function DeleteIncomeCommand({ incomesRepository }) {
   this.incomesRepository = incomesRepository;
 
   return {
-    execute: async ({ body }, res) => {
-      try {
-        const deleteIncomeResult = await this.incomesRepository.delete(body);
+    execute: async ({ body }) => {
+      const deleteIncomeResult = await this.incomesRepository.delete(body);
 
-        res.status(200).send({
-          result: deleteIncomeResult,
-        });
-      } catch (error) {
-        throw new Error(error);
-      }
+      return deleteIncomeResult;
     }
   }
 };
