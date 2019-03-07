c56c6e14e55ca0c16050b0778277fce3a95db7e9
AlexandreResende <alexandre.amres@gmail.com>
2019-03-06 22:08:14 -0300

    config: Add production config in knexfile

--- src/infrastructure/config/knexfile.js
+++ src/infrastructure/config/knexfile.js
@@ -3,6 +3,22 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
+      host: 'localhost',
+      user: 'postgrestest',
+      password: 'postgrestest',
+      database: 'poupaup',
+    },
+    debug: false,
+    migrations: {
+      directory: '../migrations'
+    },
+    seeds: {
+      directory: '../seeds'
+    }
+  },
+  production: {
+    client: process.env.DB_CLIENT || 'pg',
+    connection: {
       host: process.env.DATABASE_URL || 'localhost',
       user: 'postgrestest',
       password: 'postgrestest',
