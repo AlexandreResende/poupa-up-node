2a74ff683278f7ec5be414b339132a4f3d7744da
AlexandreResende <alexandre.amres@gmail.com>
2019-03-13 21:18:34 -0300

    chore: Update assertion.js with chai-http and chai-like libs

--- test/assertion.js
+++ test/assertion.js
@@ -1,7 +1,11 @@
 
 const chaiAsPromised = require('chai-as-promised');
+const chaiHttp = require('chai-http');
+const chaiLike = require('chai-like');
 const chai = require('chai');
 
 chai.use(chaiAsPromised);
+chai.usu(chaiHttp);
+chai.use(chaiLike);
 
 module.exports.expect = chai.expect;
